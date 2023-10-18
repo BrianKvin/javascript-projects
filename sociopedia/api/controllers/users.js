66872cae3b511b6ea24a9e6fcecda65d6549a125
BrianKvin <briankvin@gmail.com>
2023-10-18 11:44:49 +0300

    users in controllers

--- sociopedia/api/controllers/users.js
+++ sociopedia/api/controllers/users.js
@@ -17,7 +17,7 @@ export const getUserFriends = async (req, res) => {
     const user = await User.findById(id);
 
     const friends = await Promise.all(
-      user.friends.map((id) => user.findById(id))
+      user.friends.map((id) => User.findById(id))
     );
     const formattedFriends = friends.map(
       ({ _id, firstName, lastName, occupation, location, picturePath }) => {
@@ -33,13 +33,13 @@ export const getUserFriends = async (req, res) => {
 /* UPDATE */
 export const addRemoveFriend = async (req, res) => {
   try {
-    const { id, friendId } = res.params;
+    const { id, friendId } = req.params;
     const user = await User.findById(id);
     const friend = await User.findById(friendId);
 
     if (user.friends.includes(friendId)) {
       user.friends = user.friends.filter((id) => id !== friendId);
-      friend.friends = friend.friends.filter((id) => id !== userId);
+      friend.friends = friend.friends.filter((id) => id !== id);
     } else {
       user.friends.push(friendId);
       friend.friends.push(id);
@@ -47,6 +47,9 @@ export const addRemoveFriend = async (req, res) => {
     await user.save();
     await friend.save();
 
+    const friends = await Promise.all(
+      user.friends.map((id) => User.findById(id))
+    );
     const formattedFriends = friends.map(
       ({ _id, firstName, lastName, occupation, location, picturePath }) => {
         return { _id, firstName, lastName, occupation, location, picturePath };
